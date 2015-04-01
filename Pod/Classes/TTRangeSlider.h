33f19e51682adbd254678bc397c8aae18060ca18
Tom Thorpe <code@tomthorpe.co.uk>
2015-04-01 18:12:02 +0100

    fixing documentation

--- Pod/Classes/TTRangeSlider.h
+++ Pod/Classes/TTRangeSlider.h
@@ -17,36 +17,36 @@ IB_DESIGNABLE
  */
 @property (nonatomic, assign) IBInspectable float minValue;
 
-/*
+/**
  * The maximum possible value to select in the range
  */
 @property (nonatomic, assign) IBInspectable float maxValue;
 
-/*
+/**
  * The preselected minumum value
  * (note: This should be less than the selectedMaximum)
  */
 @property (nonatomic, assign) IBInspectable float selectedMinimum;
 
-/*
+/**
  * The preselected maximum value
  * (note: This should be greater than the selectedMinimum)
  */
 @property (nonatomic, assign) IBInspectable float selectedMaximum;
 
-/*
+/**
  * Each handle in the slider has a label above it showing the current selected value. By default, this is displayed as a decimal format.
  * You can override this default here by supplying your own NSNumberFormatter. For example, you could supply an NSNumberFormatter that has a currency style, or a prefix or suffix.
  * If this property is nil, the default decimal format will be used. Note: If you want no labels at all, set this value to be `(NSNumberFormatter *)[NSNull null]` (as opposed to nil), to specifically mark that you want no labels
  */
 @property (nonatomic, strong) NSNumberFormatter *numberFormatterOverride;
 
-/*
+/**
  * The color of the minimum value text label. If not set, the default is the tintColor.
  */
 @property (nonatomic, strong) IBInspectable UIColor *minLabelColour;
 
-/*
+/**
  * The color of the maximum value text label. If not set, the default is the tintColor.
  */
 @property (nonatomic, strong) IBInspectable UIColor *maxLabelColour;
