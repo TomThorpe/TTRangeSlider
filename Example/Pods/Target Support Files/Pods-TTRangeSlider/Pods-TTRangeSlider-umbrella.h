9204c71bf2dd6c008c3b709bbdd17cea482c86e9
Tom Thorpe <code@tomthorpe.co.uk>
2015-04-01 16:39:20 +0100

    adding IB_DESIGNABLE and IBInspectable so that you can simulate the control in interface builder

---
+++ Example/Pods/Target Support Files/Pods-TTRangeSlider/Pods-TTRangeSlider-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "TTRangeSlider.h"
+#import "TTRangeSliderDelegate.h"
+
+FOUNDATION_EXPORT double TTRangeSliderVersionNumber;
+FOUNDATION_EXPORT const unsigned char TTRangeSliderVersionString[];
+
