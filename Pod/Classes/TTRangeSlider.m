0fd23a92a1a96465862fb905521315c201b504e6
Eric Townsend <townsendeb@yahoo.com>
2021-04-14 07:45:13 -0500

    Added the ability to set an image for both handles

--- Pod/Classes/TTRangeSlider.m
+++ Pod/Classes/TTRangeSlider.m
@@ -658,6 +658,22 @@ static const CGFloat kLabelsFontSize = 12.0f;
     self.rightHandle.backgroundColor = [[UIColor clearColor] CGColor];
 }
 
+-(void)setSeperateHandleImages:(*UIImage *)rightHandleImage andHandle: (UIImage *)leftHandleImage{
+    _leftHandleImage = leftHandleImage
+    _rightHandleImage = rightHandleImage
+    
+    CGRect startFrame = CGRectMake(0.0, 0.0, 31, 32);
+    self.leftHandle.contents = (id)rightHandleImage.CGImage;
+    self.leftHandle.frame = startFrame;
+    
+    self.rightHandle.contents = (id)leftHandleImage.CGImage;
+    self.rightHandle.frame = startFrame;
+    
+    //Force layer background to transparant
+    self.leftHandle.backgroundColor = [[UIColor clearColor] CGColor];
+    self.rightHandle.backgroundColor = [[UIColor clearColor] CGColor];
+}
+
 -(void)setHandleColor:(UIColor *)handleColor{
     _minHandleColor = handleColor;
     _maxHandleColor = handleColor;
